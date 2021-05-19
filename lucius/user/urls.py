4c649eb7a056465fd429fae0bb4f33b4b188320b
John Adams <john@twothink.co>
2021-05-19 00:28:12 -0700

    partially working login

---
+++ lucius/user/urls.py
@@ -0,0 +1,10 @@
+# urls.py
+
+from django.urls import path, include
+from django.conf import settings
+from . import views
+from django.conf.urls.static import static
+ 
+urlpatterns = [
+    path('', views.index, name ='index'),
+]
